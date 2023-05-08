793a93d4a060035a6686a3d97b6513b0df75e1bc
shiguree47 <111887892+shiguree47@users.noreply.github.com>
2023-05-09 02:57:27 +0800

    checkHealth

--- Assets/Scripts/Enemy/CheckHealth.cs
+++ Assets/Scripts/Enemy/CheckHealth.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 using BehaviorTree;
 
+
 public class CheckHealth : Node
 {
     private Transform _transform;
@@ -11,6 +12,12 @@ public class CheckHealth : Node
     private bool reached = true;
     private float counter = 0f;
     private float waitForPlayer = 2f;
+
+
+    string heavy = "Heavy";
+    string light = "Light";
+    string melee = "Melee";
+    string range = "Range";
 
     public CheckHealth(Transform transform) 
     { 
@@ -24,50 +31,66 @@ public class CheckHealth : Node
             counter += Time.deltaTime;
             if (counter > waitForPlayer)
             {
+                
                 if (reached)
                 {
                     previousPosition = _transform.position;
                     reached = false;
                 }
+                
                 Transform target = (Transform)GetData("Player");
-
-                if(GameController.difficulty == "Easy")
+                
+                Debug.Log(Vector3.Distance(_transform.position, previousPosition));
+                if (GameController.difficulty == "Easy")
                 {
-                    if (GameController.enemyHealthAmt == 1)     //if enemy at one health, just run
+                    
+                    if (GameController.enemyHealthAmt <= 1)     //if enemy at one health, just run
                     {
-                        if (Vector3.Distance(previousPosition, _transform.position) < EnemyBT.walkDistance)
+                        if (Vector3.Distance(previousPosition, _transform.position) < EnemyBT.walkDistance)      //walk until near walk distance
                         {
+
                             GameController.enemyCurrentState = "Running";
-                            Debug.Log("tset");
                             //if health is low, 
-                            Vector3 dirToPlayer = _transform.position - target.transform.position;
+                            Vector3 dirToPlayer = _transform.position - GameController.player.transform.position;
 
                             Vector3 newPos = _transform.position + dirToPlayer;
 
                             _transform.position = Vector3.MoveTowards(_transform.position, newPos, EnemyBT.walkDistance * Time.deltaTime);
+                            _transform.LookAt(newPos);
 
+                            //Debug.Log(previousPosition);
                         }
                         else
                         {
+                            Debug.Log("Test2");
                             reached = true;
+                            counter = 0;
                             GameController.ChangeTurn();
                         }
                     }
                     else
                     {
+                        reached = true;
+                        Debug.Log("test");
                         state = NodeState.FAILURE;
                         return state;
                     }
+
                 }
                 else{//hard mode
 
                     //if enemy at one health
-                    //if enemy weapon is range and player weapon is melee, run away
-                    // if enemy type is light and enemy weapon is melee, run away
-                    //rest of conditions set to FAILURE to skip.
-
+                    if (GameController.enemyHealthAmt <= 1)
+                    {
+                        //if enemy weapon is range and player weapon is melee, run away
+                        // if enemy type is light and enemy weapon is melee, run away
+                        //rest of conditions set to FAILURE to skip.
+                        
+                    }
                     state = NodeState.FAILURE;
                     return state;
+
+
                 }
             }
         }
